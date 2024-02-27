8d84170f0b1f1e397fe4a131a0866d62220077ae
Avantha Fonseka <avanthafonseka@gmail.com>
2024-02-27 19:42:35 +0530

    login button done

---
+++ lib/components/my_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(25),
+      margin: const EdgeInsets.symmetric(horizontal: 25),
+      decoration: BoxDecoration(
+          color: Colors.black12, borderRadius: BorderRadius.circular(8)),
+      child: const Center(
+        child: Text(
+          "Log in",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sdgp_login/components/my_button.dart';
 import 'package:sdgp_login/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
@@ -61,7 +62,9 @@ class LoginPage extends StatelessWidget {
               const Text("Forgot Password?"),
 
               const SizedBox(height: 25),
+
               //login button
+              MyButton(),
 
               //sign up button
             ],
